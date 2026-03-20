8da7e024cc410f013761f11ef26dea17dfa1e38e
igorb <igorbabin2002@gmail.com>
2026-03-20 18:03:15 +0500

    Added DataContext.cs logging

--- Arch.EFCore/DataContext.cs
+++ Arch.EFCore/DataContext.cs
@@ -7,6 +7,8 @@ public class DataContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=app.db");
+        optionsBuilder.LogTo(Console.WriteLine);
+        optionsBuilder.EnableSensitiveDataLogging();
         base.OnConfiguring(optionsBuilder);
     }
     
